686373bb2347ba0044e7dfd9bb441a11d18e2d95
Noemi Balazs <balazsnoe@gmail.com>
2018-01-23 15:31:05 +0200

    Commit

--- app/src/main/java/com/example/android/michaeljordanquiz/MainActivity.java
+++ app/src/main/java/com/example/android/michaeljordanquiz/MainActivity.java
@@ -4,6 +4,9 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.accessibility.AccessibilityManager;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RatingBar;
@@ -16,10 +19,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       final CheckBox yes = (CheckBox) findViewById(R.id.yes);
+       final CheckBox no = (CheckBox) findViewById(R.id.no);
+
+        yes.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                yes.setChecked(isChecked);
+                no.setChecked(false);
+            }
+        });
+
+        no.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                no.setChecked(isChecked);
+                yes.setChecked(false);
+            }
+        });
     }
 
-
-    public void submitQuiz (View view) {
+    public void submitQuiz (final View view) {
 
         EditText eText = (EditText)findViewById(R.id.name_of_player);
         String nameOfQuizPlayer = eText.getText().toString();
@@ -28,177 +49,124 @@ public class MainActivity extends AppCompatActivity {
         String numberOfRatings = "Thank you for your evaluation. Rating: " + ratingBarJordan.getRating();
         Toast.makeText(this,numberOfRatings,Toast.LENGTH_SHORT).show();
 
-        RadioButton radioButton1962 = (RadioButton) findViewById(R.id.answer_1962);
+        RadioButton radioButton1962 = (RadioButton) findViewById(R.id.rb_answer_1962);
         boolean answerWrong1962 = radioButton1962.isChecked();
 
-        RadioButton radioButton1963 = (RadioButton) findViewById(R.id.answer_1963);
+        RadioButton radioButton1963 = (RadioButton) findViewById(R.id.rb_answer_1963);
         boolean answerCorrect1963 = radioButton1963.isChecked();
 
-        RadioButton radioButton1964 = (RadioButton) findViewById(R.id.answer_1964);
+        RadioButton radioButton1964 = (RadioButton) findViewById(R.id.rb_answer_1964);
         boolean answerWrong1964 = radioButton1964.isChecked();
 
-        RadioButton radioButton1984 = (RadioButton) findViewById(R.id.answer_1984);
+        RadioButton radioButton1984 = (RadioButton) findViewById(R.id.rb_answer_1984);
         boolean answerCorrect1984 = radioButton1984.isChecked();
 
-        RadioButton radioButton1885 = (RadioButton) findViewById(R.id.answer_1985);
+        RadioButton radioButton1885 = (RadioButton) findViewById(R.id.rb_answer_1985);
         boolean answerWrong1985 =radioButton1885.isChecked();
 
-        RadioButton radioButton1986 = (RadioButton) findViewById(R.id.answer_1986);
+        RadioButton radioButton1986 = (RadioButton) findViewById(R.id.rb_answer_1986);
         boolean answerWrong1986 = radioButton1986.isChecked();
 
-        RadioButton radioButtonHornets = (RadioButton) findViewById(R.id.answer_hornets);
+        RadioButton radioButtonHornets = (RadioButton) findViewById(R.id.rb_answer_hornets);
         boolean answerCorrectHornets = radioButtonHornets.isChecked();
 
-        RadioButton radioButtonGrizzlies = (RadioButton) findViewById(R.id.answer_grizzlies);
+        RadioButton radioButtonGrizzlies = (RadioButton) findViewById(R.id.rb_answer_grizzlies);
         boolean answerWrongGrizzlies = radioButtonGrizzlies.isChecked();
 
-        RadioButton radioButtonBucks = (RadioButton) findViewById(R.id.answer_bucks);
+        RadioButton radioButtonBucks = (RadioButton) findViewById(R.id.rb_answer_bucks);
         boolean answerWrongBucks = radioButtonBucks.isChecked();
 
-        RadioButton radioButtonJames = (RadioButton) findViewById(R.id.answer_james);
+        RadioButton radioButtonJames = (RadioButton) findViewById(R.id.rb_answer_james);
         boolean answerWrongJames = radioButtonJames.isChecked();
 
-        RadioButton radioButtonJeffrey = (RadioButton) findViewById(R.id.answer_jeffrey);
+        RadioButton radioButtonJeffrey = (RadioButton) findViewById(R.id.rb_answer_jeffrey);
         boolean answerCorrectJeffrey = radioButtonJeffrey.isChecked();
 
-        RadioButton radioButtonJohn = (RadioButton) findViewById(R.id.answer_john);
+        RadioButton radioButtonJohn = (RadioButton) findViewById(R.id.rb_answer_john);
         boolean answerWrongJohn = radioButtonJohn.isChecked();
 
-        RadioButton radioButtonOne = (RadioButton) findViewById(R.id.answer_one);
+        RadioButton radioButtonOne = (RadioButton) findViewById(R.id.rb_answer_one);
         boolean answerWrongOne = radioButtonOne.isChecked();
 
-        RadioButton radioButtonTwo = (RadioButton) findViewById(R.id.answer_two);
+        RadioButton radioButtonTwo = (RadioButton) findViewById(R.id.rb_answer_two);
         boolean answerCorrectTwo = radioButtonTwo.isChecked();
 
-        RadioButton radioButtonThree = (RadioButton) findViewById(R.id.answer_three);
+        RadioButton radioButtonThree = (RadioButton) findViewById(R.id.rb_answer_three);
         boolean answerWrongThree = radioButtonThree.isChecked();
 
-        RadioButton radioButtonFour = (RadioButton) findViewById(R.id.answer_four);
+        RadioButton radioButtonFour = (RadioButton) findViewById(R.id.rb_answer_four);
         boolean answerWrongFour = radioButtonFour.isChecked();
 
-        RadioButton radioButtonSix = (RadioButton) findViewById(R.id.answer_six);
+        RadioButton radioButtonSix = (RadioButton) findViewById(R.id.rb_answer_six);
         boolean answerCorrectSix = radioButtonSix.isChecked();
 
-        RadioButton radioButtonEight = (RadioButton) findViewById(R.id.answer_eight);
+        RadioButton radioButtonEight = (RadioButton) findViewById(R.id.rb_answer_eight);
         boolean answerWrongEight = radioButtonEight.isChecked();
 
-        RadioButton radioButton28485 = (RadioButton) findViewById(R.id.answer_28485);
+        RadioButton radioButton28485 = (RadioButton) findViewById(R.id.rb_answer_28485);
         boolean answerWrong28485 = radioButton28485.isChecked();
 
-        RadioButton radioButton30763 = (RadioButton) findViewById(R.id.answer_30763);
+        RadioButton radioButton30763 = (RadioButton) findViewById(R.id.rb_answer_30763);
         boolean answerWrong30763 = radioButton30763.isChecked();
 
-        RadioButton radioButton32292 = (RadioButton) findViewById(R.id.answer_32292);
+        RadioButton radioButton32292 = (RadioButton) findViewById(R.id.rb_answer_32292);
         boolean answerCorrect32292 = radioButton32292.isChecked();
 
-        RadioButton radioButtonAdidas = (RadioButton)findViewById(R.id.answer_adidas);
+        RadioButton radioButtonAdidas = (RadioButton)findViewById(R.id.rb_answer_adidas);
         boolean answerWrongAdidas = radioButtonAdidas.isChecked();
 
-        RadioButton radioButtonNike = (RadioButton) findViewById(R.id.answer_nike);
+        RadioButton radioButtonNike = (RadioButton) findViewById(R.id.rb_answer_nike);
         boolean answerCorrectNike = radioButtonNike.isChecked();
 
-        RadioButton radioButtonConverse = (RadioButton) findViewById(R.id.answer_converse);
+        RadioButton radioButtonConverse = (RadioButton) findViewById(R.id.rb_answer_converse);
         boolean answerWrongConverse = radioButtonConverse.isChecked();
 
-        RadioButton radioButton67 = (RadioButton) findViewById(R.id.answer_67);
+        RadioButton radioButton67 = (RadioButton) findViewById(R.id.rb_answer_67);
         boolean answerWrong67 = radioButton67.isChecked();
 
-        RadioButton radioButton68 = (RadioButton) findViewById(R.id.answer_68);
+        RadioButton radioButton68 = (RadioButton) findViewById(R.id.rb_answer_68);
         boolean answerWrong68 = radioButton68.isChecked();
 
-        RadioButton radioButton69 = (RadioButton) findViewById(R.id.answer_69);
+        RadioButton radioButton69 = (RadioButton) findViewById(R.id.rb_answer_69);
         boolean answerCorrect69 = radioButton69.isChecked();
 
         int cAnswer = calculateCorrectAnswers(answerWrong1962, answerCorrect1963, answerWrong1964, answerCorrect1984, answerWrong1985, answerWrong1986, answerCorrectHornets, answerWrongGrizzlies, answerWrongBucks, answerWrongJames, answerCorrectJeffrey, answerWrongJohn, answerWrongFour, answerCorrectSix, answerWrongEight, answerWrongOne, answerCorrectTwo, answerWrongThree, answerWrong28485, answerWrong30763, answerCorrect32292, answerWrongAdidas, answerCorrectNike, answerWrongConverse, answerWrong67, answerWrong68, answerCorrect69);
         String answerQuizPlayer = finalAnswerForQuizPlayer(cAnswer, nameOfQuizPlayer);
         displayAnswer(answerQuizPlayer);
-
     }
 
-    public int calculateCorrectAnswers (boolean addWrong1962, boolean addCorrect1963, boolean addWrong1964, boolean addCorrect1984, boolean addWrong1985, boolean addWrong1986, boolean addCorrectHornets, boolean addWrongGrizzlies, boolean addWrongBucks, boolean addWrongJames, boolean addCorrectJeffrey, boolean addWrongJohn, boolean addWrongOne, boolean addCorrectTwo, boolean addWrongThree ,boolean addWrongFour, boolean addCorrectSix, boolean addWrongEight, boolean addWrong28485, boolean addWrong30763, boolean addCorrect32292, boolean addWrongAdidas, boolean addCorrectNike, boolean addWrongConverse, boolean addWrong67, boolean addWrong68, boolean addCorrect69){
+    private int calculateCorrectAnswers (boolean addWrong1962, boolean addCorrect1963, boolean addWrong1964, boolean addCorrect1984, boolean addWrong1985, boolean addWrong1986, boolean addCorrectHornets, boolean addWrongGrizzlies, boolean addWrongBucks, boolean addWrongJames, boolean addCorrectJeffrey, boolean addWrongJohn, boolean addWrongOne, boolean addCorrectTwo, boolean addWrongThree ,boolean addWrongFour, boolean addCorrectSix, boolean addWrongEight, boolean addWrong28485, boolean addWrong30763, boolean addCorrect32292, boolean addWrongAdidas, boolean addCorrectNike, boolean addWrongConverse, boolean addWrong67, boolean addWrong68, boolean addCorrect69){
         int correctAnswer = 0;
-        if (addWrong1962){
-            correctAnswer =0;
-        }
         if(addCorrect1963){
-            correctAnswer = correctAnswer + 1;
-        }
-        if (addWrong1964){
-            correctAnswer = 0;
+            correctAnswer += 1;
         }
         if(addCorrect1984){
-            correctAnswer = correctAnswer + 1;
-        }
-        if(addWrong1985) {
-            correctAnswer = 0;
-        }
-        if(addWrong1986){
-            correctAnswer = 0;
+            correctAnswer += 1;
         }
         if(addCorrectHornets){
-            correctAnswer = correctAnswer + 1;
-        }
-        if(addWrongGrizzlies){
-            correctAnswer = 0;
-        }
-        if(addWrongBucks){
-            correctAnswer = 0;
-        }
-        if(addWrongJames){
-            correctAnswer = 0;
+            correctAnswer += 1;
         }
         if(addCorrectJeffrey){
-            correctAnswer = correctAnswer + 1;
-        }
-        if(addWrongJohn){
-            correctAnswer = 0;
-        }
-        if(addWrongOne){
-            correctAnswer= 0;
+            correctAnswer += 1;
         }
         if(addCorrectTwo){
-            correctAnswer = correctAnswer + 1;
-        }
-        if(addWrongThree){
-            correctAnswer = 0;
-        }
-        if(addWrongFour){
-            correctAnswer = 0;
+            correctAnswer += 1;
         }
         if(addCorrectSix){
-            correctAnswer = correctAnswer + 1;
-        }
-        if(addWrongEight){
-            correctAnswer = 0;
-        }
-        if (addWrong28485){
-            correctAnswer = 0;
-        }
-        if(addWrong30763){
-            correctAnswer = 0;
+            correctAnswer += 1;
         }
         if(addCorrect32292){
-            correctAnswer = correctAnswer + 1;
-        }
-        if(addWrongAdidas){
-            correctAnswer = 0;
+            correctAnswer += 1;
         }
         if(addCorrectNike){
-            correctAnswer = correctAnswer + 1;
-        }
-        if(addWrongConverse){
-            correctAnswer = 0;
-        }
-        if(addWrong67){
-            correctAnswer = 0;
-        }
-        if(addWrong68){
-            correctAnswer = 0;
+            correctAnswer += 1;
         }
         if(addCorrect69){
-            correctAnswer = correctAnswer + 1;
+            correctAnswer += 1;
         }
-
+        else {
+            correctAnswer =0;
+        }
         return correctAnswer;
     }
 
@@ -212,6 +180,20 @@ public class MainActivity extends AppCompatActivity {
         yourScore.setText(answer);
     }
 
-
+    public void onPressed(View view){
+        boolean checked = ((CheckBox) view).isChecked();
+        switch (view.getId()){
+            case R.id.yes:
+                if (checked){
+                    Toast.makeText(this, "I'm glad you enjoyed my quiz", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.no:
+                if (checked){
+                    Toast.makeText(this, "I'm sorry! Probably you have very high expectations!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
 }
 
